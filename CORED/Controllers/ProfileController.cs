c1356cbafa59d87e8b1093ee754962f6c3e6452a
Joshua Mayo <Joshua.Mayo@olmgroup.com>
2019-01-21 17:33:44 +0000

    added widget

---
+++ CORED/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CORED.Models;
+
+namespace CORED.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index(ProfileVm model)
+        {
+            return View();
+        }
+    }
+}
