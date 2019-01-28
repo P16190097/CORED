f42e90bd5636a93c0d50f3b36d477e6d0f6a2a19
Joshua Mayo <Joshua.Mayo@olmgroup.com>
2019-01-28 17:13:01 +0000

    added simple form +bootstrap cleaning

--- CORED/Controllers/ProfileController.cs
+++ CORED/Controllers/ProfileController.cs
@@ -9,9 +9,30 @@ namespace CORED.Controllers
 {
     public class ProfileController : Controller
     {
-        public IActionResult Index(ProfileVm model)
+        private static readonly string Error = "Please check details again";
+
+        public ActionResult Index(ProfileVm model)
         {
+            return View("Signup", model);
+        }
+
+        [HttpGet]
+        public IActionResult Signup()
+        {
+            ViewData["Message"] = "Create your account here";
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(ProfileVm model)
+        {
+            if(!ModelState.IsValid)
+            {
+                ModelState.AddModelError(string.Empty, Error);
+                return View("Signup", model);
+            }
+            return View("Signup", model);
         }
     }
 }
